d02ed412f25391f0fd20310fabde2bdd00f69d07
rjcculaway <rcculaway@up.edu.ph>
2020-02-04 16:36:55 +0800

    Commented the old code instead of removed

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -21,30 +21,27 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener   { rollDice() }
 
-        val countUpButton: Button = findViewById(R.id.count_up)
-        countUpButton.setOnClickListener   { countUp() }
+//        val countUpButton: Button = findViewById(R.id.count_up)
+//        countUpButton.setOnClickListener   { countUp() }
     }
 
     private fun rollDice() {
         // Randomize a number
         val randomInt = (1..6).random()
-        // Assign that number
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
     }
 
-    private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        val txt = resultText.text.toString()
-
-        // Try and increment the number on the dice.
-        try {
-            var num = txt.toInt() + 1
-            if (num < 7) {
-                resultText.text = num.toString()
-            }
-        } catch (e:NumberFormatException) {
-            resultText.text = "1"
-        }
-    }
+//    private fun countUp() {
+//        val resultText: TextView = findViewById(R.id.result_text)
+//        val txt = resultText.text.toString()
+//
+//        // Try and increment the number on the dice.
+//        try {
+//            var num = txt.toInt() + 1
+//            if (num < 7) {
+//                resultText.text = num.toString()
+//            }
+//        } catch (e:NumberFormatException) {
+//            resultText.text = "1"
+//        }
+//    }
 }
